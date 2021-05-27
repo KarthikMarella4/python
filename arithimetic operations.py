85bcadf6b47a9a8544b4a684e4cd5a9e4f06e7df
karthikmarella24 <80272758+karthikmarella24@users.noreply.github.com>
2021-05-27 21:06:19 +0530

    Add files via upload

---
+++ arithimetic operations.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter a value:"))
+num2=int(input("Enter a value:"))
+add=num1+num2
+print("add:",add)
+sub=num1-num2
+print("sub:",sub)
+mult=num1*num2
+print("mult:",mult)
+div=num1%num2
+print("div:",div)
+pow=num1**num2
+print("pow:",pow)
+
